219d4cc0432a970ef9ad532f0f43cefd0fda2d38
Shyam Choudhary <shyamg63v8@gmail.com>
2025-04-07 11:34:18 +0530

    updated cart

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,30 +2,41 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import NavIcons from "./navicons";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [open]);
+
   return (
     <div className="">
-      <Image
-        src="/menu.png"
-        alt=""
-        width={28}
-        height={28}
-        className="cursor-pointer"
-        onClick={() => setOpen((prev) => !prev)}
-      />
+      <div className="flex items-center gap-2">
+        <Image
+          src="/menu.png"
+          alt=""
+          width={28}
+          height={28}
+          className="cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        />
+        <NavIcons />
+      </div>
       {open && (
-        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl  z-10">
+        <div className="absolute bg-gray-200  left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl  z-10">
           <Link href="/">Homepage</Link>
           <Link href="/">Shop</Link>
           <Link href="/">Deals</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
           <Link href="/">Logout</Link>
-          <Link href="/">Cart(1)</Link>
         </div>
       )}
     </div>
